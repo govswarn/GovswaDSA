b543e79e67ff51587b9997e7d23eede0a3a199c1
GovilSwarnkar <aryagovilswarnnkar@gmail.com>
2026-05-21 03:18:03 +0530

    array-easy

--- src/main/java/com/govswa/dsa/array/MaxConsecutiveOnes.java
+++ src/main/java/com/govswa/dsa/array/MaxConsecutiveOnes.java
@@ -2,18 +2,37 @@ package com.govswa.dsa.array;
 
 public class MaxConsecutiveOnes {
 
-    public static int maxConsecutiveOnes(int[] a) {
-        int maxC = Integer.MIN_VALUE;
-        for (int i = 0; i < a.length; i++) {
-            int c = 0;
-            int j = i;
+    public static int maxConsecutiveOnes_1(int[] a) {
+        int c, maxC = Integer.MIN_VALUE;
+        int i = 0;
+        int j;
+        while (i < a.length) {
+            c = 0;
+            j = i;
             while (j < a.length && a[j] == 1) {
                 c++;
                 j++;
             }
             if (maxC < c) {
                 maxC = c;
+                i = j;
+            } else {
+                i++;
             }
+
+        }
+        return maxC;
+    }
+
+    public static int maxConsecutiveOnes(int[] a) {
+        int c = 0, maxC = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == 1) {
+                c++;
+            } else {
+                c = 0;
+            }
+            maxC = Math.max(maxC, c);
         }
         return maxC;
     }
@@ -22,6 +41,12 @@ public class MaxConsecutiveOnes {
         int[] a1 = {1, 0, 1, 1, 1, 0, 1, 1};
         int[] a2 = {1, 1, 0, 0, 1, 1, 0, 0};
         int[] a3 = {1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0};
+
+        System.out.println(maxConsecutiveOnes_1(a1));
+        System.out.println(maxConsecutiveOnes_1(a2));
+        System.out.println(maxConsecutiveOnes_1(a3));
+
+        System.out.println("------------------------------");
         System.out.println(maxConsecutiveOnes(a1));
         System.out.println(maxConsecutiveOnes(a2));
         System.out.println(maxConsecutiveOnes(a3));
